98956fa26dd5470db4b2eb8119bf7956778969e8
pioncha2 <pioncha2@student.42.fr>
2025-11-01 13:11:32 +0700

    Remove unnecessary file descriptor closures in clean_cmd_node function

--- src/clean_cmd_group.c
+++ src/clean_cmd_group.c
@@ -6,7 +6,7 @@
 /*   By: pioncha2 <pioncha2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 21:06:18 by pioncha2          #+#    #+#             */
-/*   Updated: 2025/11/01 12:40:54 by pioncha2         ###   ########.fr       */
+/*   Updated: 2025/11/01 13:08:42 by pioncha2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,13 +50,6 @@ static void	clean_cmd_node(t_cmd_group *node)
 	free_tab(node->out_filenames);
 	free(node->cmd);
 	free_tab(node->argv);
-	close_fd(node->in_fd);
-	close_fd(node->out_fd);
-	if (node->is_heredoc)
-	{
-		close_fd(node->h_pipe[0]);
-		close_fd(node->h_pipe[1]);
-	}
 	clean_infiles_list(node->in_files);
 	clean_outfiles_list(node->out_files);
 }
