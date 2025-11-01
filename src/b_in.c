cfa3c864fbe1ebbf0f45f766291e849429169de0
WaroonRagwongsiri <waroonwork@gmail.com>
2025-11-01 20:31:59 +0700

    Ok

--- src/b_in.c
+++ src/b_in.c
@@ -1,28 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   end.c                                              :+:      :+:    :+:   */
+/*   b_in.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: waroonwork@gmail.com <WaroonRagwongsiri    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/31 16:26:20 by waragwon          #+#    #+#             */
-/*   Updated: 2025/11/01 20:31:48 by waroonwork@      ###   ########.fr       */
+/*   Created: 2025/11/01 20:28:54 by waroonwork@       #+#    #+#             */
+/*   Updated: 2025/11/01 20:28:59 by waroonwork@      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	exit_msg(char *msg)
-{
-	ft_putendl_fd(msg, 2);
-	ft_safe_calloc(-1, -1, NULL);
-	exit(EXIT_FAILURE);
-}
-
-void	exit_cmd(char *cmd)
-{
-	ft_putstr_fd(cmd, STDERR_FILENO);
-	ft_putendl_fd(": command not found", STDERR_FILENO);
-	ft_safe_calloc(-1, -1, NULL);
-	exit(127);
-}
