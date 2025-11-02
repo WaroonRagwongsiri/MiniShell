3b2b33449a1a049729871af9f618326962ee99ec
Waroon Ragwongsiri <waragwon@z2t9c1.42bangkok.com>
2025-11-02 15:00:14 +0700

    normminette

--- src/signal.c
+++ src/signal.c
@@ -6,7 +6,7 @@
 /*   By: waragwon <waragwon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 14:17:50 by waragwon          #+#    #+#             */
-/*   Updated: 2025/11/02 14:32:11 by waragwon         ###   ########.fr       */
+/*   Updated: 2025/11/02 14:59:00 by waragwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,17 @@ volatile sig_atomic_t	g_status = 0;
 void	handler(int signum)
 {
 	if (signum == SIGINT)
-		exit_msg("Interrupt");
+	{
+		rl_on_new_line();
+		rl_replace_line("", 1);
+		rl_crlf();
+		rl_set_prompt(get_prompt(NULL));
+		rl_redisplay();
+	}
 	else if (signum == SIGQUIT)
-		exit_msg("Quit");
+		continue ;
 	else if (signum == SIGTSTP)
+	{
 		exit_msg("Stop");
+	}
 }
