34168dbb4f2660e2cc77a69ff69594e967c262ad
Waroon Ragwongsiri <waragwon@z2t9c1.42bangkok.com>
2025-11-04 17:32:56 +0700

    Fix SHLVL

--- inc/minishell.h
+++ inc/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pioncha2 <pioncha2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: waragwon <waragwon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 10:11:15 by pioncha2          #+#    #+#             */
-/*   Updated: 2025/11/04 17:13:59 by pioncha2         ###   ########.fr       */
+/*   Updated: 2025/11/04 17:39:48 by waragwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,6 +111,7 @@ int			set_env_var(char ***env, char *arg);
 
 // builtin_utils2.c
 bool		is_valid_echo_flag(char *arg);
+char		*get_env_value(char **env, const char *key);
 
 // exec_buildin.c
 int			is_builtin(char *cmd);
--- src/builtin_utils2.c
+++ src/builtin_utils2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_utils2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pioncha2 <pioncha2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: waragwon <waragwon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 07:56:23 by pioncha2          #+#    #+#             */
-/*   Updated: 2025/11/04 17:16:45 by pioncha2         ###   ########.fr       */
+/*   Updated: 2025/11/04 17:40:19 by waragwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,27 @@ bool	is_valid_echo_flag(char *arg)
 		i++;
 	}
 	return (true);
+}
+
+char	*get_env_value(char **env, const char *key)
+{
+	int		i;
+	size_t	key_len;
+
+	if (env == NULL || key == NULL || key[0] == '\0')
+		return (NULL);
+	key_len = ft_strlen(key);
+	i = 0;
+	while (env[i] != NULL)
+	{
+		if (ft_strncmp(env[i], key, key_len) == 0)
+		{
+			if (env[i][key_len] == '=')
+				return (&env[i][key_len + 1]);
+			else if (env[i][key_len] == '\0')
+				return ("");
+		}
+		i++;
+	}
+	return (NULL);
 }
